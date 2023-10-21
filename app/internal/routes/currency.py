f3d7c3b13b14ac6873973a54e4650c4e272f5697
mbgoodguy <nerooren4001@gmail.com>
2023-10-21 16:00:20 +0300

    Add redis connection instrument
    Add route for data get

---
+++ app/internal/routes/currency.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from app.package.redis_tools.tools import RedisTools
+
+router = APIRouter(
+    prefix='/api/v1/currency'
+)
+
+
+@router.get('/{pair}')
+def currency_pair(pair: str):
+    if pair not in [s.encode('utf-8') for s in RedisTools.get_keys()]:
+        return {'error': 'This pair does not exists'}
+
+    return {
+        'pair': pair,
+        'price': RedisTools.get_pair(pair)
+    }
+
+
