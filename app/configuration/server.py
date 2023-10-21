061823af7ee4cc3e4483e99f8d3ac18837887d4e
mbgoodguy <nerooren4001@gmail.com>
2023-10-21 18:18:35 +0300

    (fix) Add self.__register_events(app) into server.py

--- app/configuration/server.py
+++ app/configuration/server.py
@@ -10,15 +10,16 @@ class Server:
     def __init__(self, app: FastAPI):
         self.__app = app
         self.__register_routes(app)
+        self.__register_events(app)
 
     def get_app(self) -> FastAPI:
         return self.__app
 
     @staticmethod
-    def __register_routes(app):
-        __routes__.register_routes(app)
-
-    @staticmethod
     def __register_events(app):
         app.on_event('startup')(repeat_every(seconds=60 * 60 * 12)(on_startup))
         app.on_event('startup')(repeat_every(seconds=5)(on_loop_startup))
+
+    @staticmethod
+    def __register_routes(app):
+        __routes__.register_routes(app)
